461136748b710237bec7b0e0b443887c35bf3a85
aryashuklacs24-dev <arya.shuklacs24@gmail.com>
2026-03-19 22:42:44 +0530

    lec4

---
+++ lecture_4/bin_to_dec.java
@@ -0,0 +1,18 @@
+public class bin_to_dec {
+
+	public static void main(String[] args) {
+		int n = 10011;
+		int sum = 0;
+		int mul = 1;
+		while (n > 0) {
+			int rem = n % 10;
+			sum = sum + rem * mul;
+			mul = mul * 2;
+			n = n / 10;
+
+		}
+		System.out.println(sum);
+	}
+
+}
+
