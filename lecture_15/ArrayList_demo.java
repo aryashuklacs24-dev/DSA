6cbfd377907295b8d4be80301127e67ee54b355b
aryashuklacs24-dev <arya.shuklacs24@gmail.com>
2026-04-01 23:59:30 +0530

    ArrayList and Wrapper Class

---
+++ lecture_15/ArrayList_demo.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class ArrayList_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> ll = new ArrayList<>();
+		System.out.println(ll.size());
+		// Add 
+		ll.add(10);
+		ll.add(20);
+		ll.add(30);
+		System.out.println(ll);
+		ll.add(1, -7);// 
+		System.out.println(ll);
+		// get
+		System.out.println(ll.get(1));
+		// remove
+		System.out.println(ll.remove(0));
+		System.out.println(ll);
+		// update
+		ll.set(1, 110);
+		System.out.println(ll);
+		Collections.sort(ll);
+		System.out.println(ll);
+	}
+
+}
---
+++ lecture_15/demo3.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> ll = new ArrayList<>();
+		// Add
+		ll.add(10);
+		ll.add(20);
+		ll.add(30);
+		ll.add(11);
+		ll.add(2);
+		ll.add(30);
+		int[] arr = new int[4];
+		for (int x : arr) {
+			System.out.print(x+" ");
+		}
+		System.out.println();
+		for (int x : ll) {
+			System.out.print(x+" ");
+		}
+	}
+
+
+}
