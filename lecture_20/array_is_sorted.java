fc9afc5e6c48c3d43491762d92c092c7c667cec0
aryashuklacs24-dev <arya.shuklacs24@gmail.com>
2026-04-11 21:50:29 +0530

    Recursion 2

---
+++ lecture_20/array_is_sorted.java
@@ -0,0 +1,20 @@
+public class array_is_sorted {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] arr = {2,3,5,8,7};
+		System.out.println(Is_Sorted(arr, 0));
+
+	}
+	public static boolean Is_Sorted(int [] arr,int i) {
+		if(i==arr.length-1) {
+			return true;
+		}
+		if(arr[i]>arr[i+1]) {
+			return false;
+		}
+		return Is_Sorted(arr, i+1);
+	}
+
+
+}
