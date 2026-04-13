c68a78e7782eacad716819a01bece468d6ae2b90
aryashuklacs24-dev <arya.shuklacs24@gmail.com>
2026-04-13 23:04:08 +0530

    Recursion

---
+++ lecture_21/coin_toss2.java
@@ -0,0 +1,20 @@
+public class coin_toss2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 4;
+		Print(n, "");
+
+	}
+
+	public static void Print(int n, String ans) {
+		if (n == 0) {
+			System.out.println(ans);
+			return;
+		}
+		if (ans.length()==0 ||ans.charAt(ans.length() - 1) != 'H') {
+			Print(n - 1, ans + "H");
+		}
+		Print(n - 1, ans + "T");
+	}
+
+}
---
+++ lecture_21/print_subsequence.java
@@ -0,0 +1,22 @@
+public class print_subsequence {
+
+	public static void main(String[] args) {
+		String ques = "abc";
+		Print(ques, "");
+
+	}
+
+	public static void Print(String ques, String ans) {
+		if (ques.length() == 0) {
+			System.out.println(ans);
+			return;
+		}
+
+		char ch = ques.charAt(0);
+		Print(ques.substring(1), ans);
+		Print(ques.substring(1), ans + ch);
+
+	}
+
+
+}
