21768310c8c0780612266e873e6b78f4f0d915bf
aryashuklacs24-dev <arya.shuklacs24@gmail.com>
2026-04-19 12:10:22 +0530

    Recursion and Backtracking

---
+++ lecture_24/coin_permutation.java
@@ -0,0 +1,28 @@
+public class coin_permutation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] coin = { 2, 3, 5 };
+		int ammount = 8;
+        Permutation(coin, ammount, "");
+
+	}
+
+	public static void Permutation(int[] coin, int ammount, String ans) {
+		if (ammount == 0) {
+			System.out.println(ans);
+			return;
+		}
+
+		for (int i = 0; i < coin.length; i++) {
+			if (ammount >= coin[i]) {
+				
+				Permutation(coin, ammount - coin[i], ans + coin[i]);
+			}
+
+		}
+
+	}
+
+
+}
