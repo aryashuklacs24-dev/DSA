09535ccb92c1bd91dfa9b74a41102609645caa2c
aryashuklacs24-dev <arya.shuklacs24@gmail.com>
2026-05-15 12:09:51 +0530

    LinkedList

---
+++ lecture_36/LinkedList_Client.java
@@ -0,0 +1,12 @@
+public class LinkedList_Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList ll = new LinkedList();
+		ll.addfirst(10);
+		ll.addfirst(20);
+		ll.addfirst(30);
+
+	}
+
+}
